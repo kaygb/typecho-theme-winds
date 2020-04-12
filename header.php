1aeb7f607be093a261824e9adce6e8dbf7511820
kaygb <48642860+kaygb@users.noreply.github.com>
2020-04-12 20:56:49 +0800

    禁用缓存

--- header.php
+++ header.php
@@ -5,23 +5,30 @@
     <meta charset="<?php $this->options->charset(); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
+
     <!-- 新 Bootstrap4 核心 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- font-awsome cdn -->
     <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/5.13.0/css/all.min.css">
 
+    <!-- 布局·WINDS样式 -->
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/header.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/winds.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/layout.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/sidebar.css'); ?>">
 
-
+    <!-- 测试-禁用缓存 -->
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <!-- 通过自有函数输出HTML头部信息 -->
     <?php $this->header(); ?>
 </head>
