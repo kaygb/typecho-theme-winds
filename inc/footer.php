2ab3a9e1f272d08fc5973e46bc189cb0cc0f1738
kaygb <48642860+kaygb@users.noreply.github.com>
2020-04-15 10:28:08 +0800

    footer高度调整

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,14 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div class="footer container-fluid blur">
 <footer id="footer" class="container" role="contentinfo">
+
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    <?php _e('Powered by <a class="text-primary" href="https://www.typecho.org" target="_blank">Typecho</a> and theme  <a class="text-primary" href="https://github.com/kaygb/typecho-theme-winds" target="_blank">winds</a> '); ?>.
+    <br>
+    <?php _e('Powered by <a class="text-success" href="https://www.typecho.org" target="_blank">Typecho</a> and theme  <a class="text-success" href="https://github.com/kaygb/typecho-theme-winds" target="_blank">WINDS</a> '); ?>.
+    <br>
+    <a href="#">没ICP备12345678号</a>
+
+
 </footer><!-- end #footer -->
 </div>
 <?php $this->footer(); ?>
