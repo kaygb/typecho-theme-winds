1469e7e0ab3583b7280ab78346c8f586488c6f4a
kaygb <48642860+kaygb@users.noreply.github.com>
2020-04-15 12:24:12 +0800

    侧边栏样式begin

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,4 +1,12 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;?>
-<div class="col-lg-4">  
-    <div class="profile">侧栏</div> 
+<div class="col-lg-4 sidebar">  
+    <div class="profile">
+        <img class="wavatar" src="https://file.kaygb.top/static_image/kaygb-logo/tx.png" alt="" srcset="">
+        <div class="social">
+            <span>HOME</span>
+            <span>GITHUB</span>
+            <span>BILIBILI</span>
+            <span>WEIBO</span>
+        </div>
+    </div> 
 </div>
