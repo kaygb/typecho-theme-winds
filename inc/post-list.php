3e42e96b53501971d82851a512d7dc4ec5757c04
kaygb <48642860+kaygb@users.noreply.github.com>
2020-04-17 16:26:33 +0800

    模块化

---
+++ inc/post-list.php
@@ -0,0 +1,34 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+// 文章列表
+?>
+
+<?php if($this->fields->postThumbUrl !=""): ?>
+                <div class="thumb-panle">
+                    <a href="<?php $this->permalink() ?>">
+                        <div class="thumb-img" style="background-image: url(<?php echo $this->fields->postThumbUrl;?> )">
+                        </div>
+                    </a>
+                        
+                </div>
+                <? endif;?>
+
+
+
+                <div class="posts-winds">
+                    <h2 class="post-title">
+                        <a href="<?php $this->permalink() ?>">
+                            <?php $this->title() ?>
+                        </a>
+                    </h2>
+                    <!-- <div class="post-content">
+                    <?php //$this->excerpt(50,''); ?>
+                    </div> -->
+                    <div class="post-meta">
+                        <span class="text-white bg-success">  
+                            <?php _e('<i class="fas fa-calendar-alt"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
+                        </span>
+                        <span class="bg-danger text-white">
+                            <?php _e('<i class="fas fa-folder-minus"></i> '); ?><?php $this->category(','); ?>
+                        </span>
+                    </div>
+                </div>
--- inc/posts.php
+++ inc/posts.php
@@ -9,36 +9,8 @@
         <?php //$this->need('inc/carousel.php'); ?>
             <?php while($this->next()): ?>
             <article class="posts">    
-                <?php if($this->fields->postThumbUrl !=""): ?>
-                <div class="thumb-panle">
-                    <a href="<?php $this->permalink() ?>">
-                        <div class="thumb-img" style="background-image: url(<?php echo $this->fields->postThumbUrl;?> )">
-                        </div>
-                    </a>
-                        
-                </div>
-                <? endif;?>
-
-
-
-                <div class="posts-winds">
-                    <h2 class="post-title">
-                        <a href="<?php $this->permalink() ?>">
-                            <?php $this->title() ?>
-                        </a>
-                    </h2>
-                    <div class="post-content">
-                    <?php $this->excerpt(50,''); ?>
-                    </div>
-                    <div class="post-meta">
-                        <span class="text-white bg-success">  
-                            <?php _e('<i class="fas fa-calendar-alt"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
-                        </span>
-                        <span class="bg-danger text-white">
-                            <?php _e('<i class="fas fa-folder-minus"></i> '); ?><?php $this->category(','); ?>
-                        </span>
-                    </div>
-                </div>
+            <?php $this->need('inc/post-list.php'); ?>
+            
             </article>
             <?php endwhile; ?>
             <div id="w-pagination" class="w-pagination">
