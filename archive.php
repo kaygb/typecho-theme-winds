668fc005a5028d35b1f99637c474ec2ecffdcb01
kaygb <48642860+kaygb@users.noreply.github.com>
2020-04-18 17:38:02 +0800

    delete OLD

--- archive.php
+++ archive.php
@@ -12,37 +12,8 @@
         ), '', ''); ?></h3>
         <?php if ($this->have()): ?>
     	<?php while($this->next()): ?>
-            <article class="posts" itemscope itemtype="http://schema.org/BlogPosting">
-            <?php if($this->fields->postThumbUrl !=""): ?>
-                <div class="thumb-panle">
-                    <a href="<?php $this->permalink() ?>">
-                        <div class="thumb-img" style="background-image: url(<?php echo $this->fields->postThumbUrl;?> )">
-                        </div>
-                    </a>
-                        
-                </div>
-                <? endif;?>
-
-
-
-                <div class="posts-winds">
-                    <h2 class="post-title">
-                        <a href="<?php $this->permalink() ?>">
-                            <?php $this->title() ?>
-                        </a>
-                    </h2>
-                    <div class="post-content">
-                    <?php $this->excerpt(50,''); ?>
-                    </div>
-                    <div class="post-meta">
-                        <span class="text-white bg-success">  
-                            <?php _e('<i class="fas fa-calendar-alt"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
-                        </span>
-                        <span class="bg-danger text-white">
-                            <?php _e('<i class="fas fa-folder-minus"></i> '); ?><?php $this->category(','); ?>
-                        </span>
-                    </div>
-                </div>
+            <article class="posts card-show" itemscope itemtype="http://schema.org/BlogPosting">
+            <?php $this->need('inc/post-list.php'); ?>
     		</article>
     	<?php endwhile; ?>
         <?php else: ?>
