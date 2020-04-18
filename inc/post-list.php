9c755204801478fe2a764807c6de58f6414febd2
kaygb <48642860+kaygb@users.noreply.github.com>
2020-04-18 16:42:55 +0800

    Update post-list.php

--- inc/post-list.php
+++ inc/post-list.php
@@ -2,33 +2,30 @@
 // 文章列表
 ?>
 
-<?php if($this->fields->postThumbUrl !=""): ?>
-                <div class="thumb-panle">
-                    <a href="<?php $this->permalink() ?>">
-                        <div class="thumb-img" style="background-image: url(<?php echo $this->fields->postThumbUrl;?> )">
-                        </div>
-                    </a>
-                        
-                </div>
-                <? endif;?>
-
-
-
-                <div class="posts-winds">
-                    <h2 class="post-title">
-                        <a href="<?php $this->permalink() ?>">
-                            <?php $this->title() ?>
-                        </a>
-                    </h2>
-                    <div class="post-content">
-                    <?php $this->excerpt(200,''); ?>
-                    </div>
-                    <div class="post-meta">
-                        <span class="text-white bg-success">  
-                            <?php _e('<i class="fas fa-calendar-alt"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
-                        </span>
-                        <span class="bg-danger text-white">
-                            <?php _e('<i class="fas fa-folder-minus"></i> '); ?><?php $this->category(','); ?>
-                        </span>
-                    </div>
-                </div>
+    <?php if($this->fields->postThumbUrl !=""): ?>
+    <div class="thumb-panle">
+        <a href="<?php $this->permalink() ?>">
+            <div class="thumb-img" style="background-image: url(<?php echo $this->fields->postThumbUrl;?> )">
+            </div>
+        </a>
+            
+    </div>
+    <? endif;?>
+    <div class="posts-winds">
+        <h2 class="post-title">
+            <a href="<?php $this->permalink() ?>">
+                <?php $this->title() ?>
+            </a>
+        </h2>
+        <div class="post-content">
+        <?php $this->excerpt(200,''); ?>
+        </div>
+        <div class="post-meta">
+            <span class="text-white bg-success">  
+                <?php _e('<i class="fas fa-calendar-alt"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
+            </span>
+            <span class="bg-danger text-white">
+                <?php _e('<i class="fas fa-folder-minus"></i> '); ?><?php $this->category(','); ?>
+            </span>
+        </div>
+    </div>
