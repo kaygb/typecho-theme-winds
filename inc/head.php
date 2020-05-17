f25014f5b1096d95df55e87a7a35dec40dd1bec1
乔千 <61873808+MliKiowa@users.noreply.github.com>
2020-05-17 14:05:58 +0800

    部署正式环境 调整编码设计
    默认utf8(全球化标准)
    删除无缓存测试代码

--- inc/head.php
+++ inc/head.php
@@ -15,7 +15,7 @@
 <!DOCTYPE HTML>
 <html class="no-js">
 <head>
-    <meta charset="<?php $this->options->charset(); ?>">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -42,15 +42,8 @@
     ?>
         
     </title>
-
-
-    <!-- 新 Bootstrap4 核心 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
-
-    <!-- font-awsome cdn -->
     <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/5.13.0/css/all.min.css">
-
-    <!-- 布局·WINDS样式 -->
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/header.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/winds.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/layout.css'); ?>">
@@ -58,32 +51,18 @@
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/post.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/footer.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/keyframe.css'); ?>">
-<!-- highlightjs -->
-<link rel="stylesheet"
+    <link rel="stylesheet"
       href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.18.1/build/styles/atom-one-dark.min.css">
-<!-- fancybox -->
-      <link rel="stylesheet" href="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.css">
-<!-- tocify -->
+    <link rel="stylesheet" href="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.css">
 <!-- <link rel="stylesheet" href="https://cdn.staticfile.org/jquery.tocify/1.9.0/stylesheets/jquery.tocify.min.css"> -->
-
 <!-- autoToc -->
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kaygb/blogMenu@master/css/jquery.autoMenu.css"> -->
       <!-- google-fonts -->
       <!-- <link href='https://fonts.loli.net/css?family=Open+Sans' rel='stylesheet'> -->
-
-    <!-- 测试-禁用缓存 -->
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
     <!-- 通过自有函数输出HTML头部信息 -->
     <?php $this->header(); ?>
 </head>
 <body>
-<!--[if lt IE 8]>
-    <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
-<![endif]-->
-
-
 <?php $this->need('inc/banner.php');?>
 
 
