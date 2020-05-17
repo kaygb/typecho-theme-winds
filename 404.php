0922b9277f56bad8b4f353d4a2b94be56b880782
乔千 <61873808+MliKiowa@users.noreply.github.com>
2020-05-17 13:56:29 +0800

    更新注释和计划
    重新注释

--- 404.php
+++ 404.php
@@ -5,17 +5,13 @@
 *   使用方式见：https://eas1.cn/167.html
 */
 
-// $error_url = '//'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-// $id = strstr($error_url, 'posts');
-// if($id != "" ||$id != null){//判断是否不等于空或者null
-//     $nameid = substr($id,5);// 截取posts之后的字符串
-//       header('HTTP/1.1 301 Moved Permanently'); //添加301状态码
-//       header("location://".$_SERVER['HTTP_HOST'].$nameid);//跳转页面    
-//   }
+//标注:此处404状态码 可以通过来源获取生成页面js 或者通过js
+//js历史记录操作回调正常页面 当然也可以选择调回主页 debug模式会导致404页面消失
 ?>
 <?php $this->need('inc/head.php');
  $this->need('inc/header.php'); ?>
 <div class="main container">
+	
 </div>
     
 	<?php $this->need('inc/footer.php'); ?>
