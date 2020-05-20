e42bf22f37f86235a30613d0b62c7088b071876b
kaygb <48642860+kaygb@users.noreply.github.com>
2020-05-20 15:59:09 +0800

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -15,7 +15,7 @@
 <!DOCTYPE HTML>
 <html class="no-js">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php $this->options->charset(); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
