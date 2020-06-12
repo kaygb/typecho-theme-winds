623bcf3d5b7ea4c2374bb6b8dca8fa6325e0595d
kaygb <kaygbtop@163.com>
2020-06-12 11:51:02 +0800

    更换图片加载动画，添加404固定链接跳转开关

--- 404.php
+++ 404.php
@@ -1,17 +1,19 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php
 /*
-*   这里的一小块代码为固定链接跳转
+*   这里的一小块代码为固定链接跳转,默认关闭，如有需要请自行修改
 *   使用方式见：https://eas1.cn/167.html
 */
-$error_url = '//'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-$id = strstr($error_url, 'posts');    //截取以posts开始的字符串
-if($id != "" ||$id != null){//判断是否不等于空或者null
-    $nameid = substr($id,5);// 截取posts之后的字符串
-      header('HTTP/1.1 301 Moved Permanently'); //添加301状态码
-      header("location://".$_SERVER['HTTP_HOST'].$nameid);//跳转页面    
-  }
-
+if ($this->options->winds301 == 1){
+  // echo "1111111";
+  $error_url = '//'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+  $id = strstr($error_url, 'posts');    //截取以posts开始的字符串
+  if($id != "" ||$id != null){//判断是否不等于空或者null
+      $nameid = substr($id,5);// 截取posts之后的字符串
+        header('HTTP/1.1 301 Moved Permanently'); //添加301状态码
+        header("location://".$_SERVER['HTTP_HOST'].$nameid);//跳转页面    
+    }
+}
 ?>
 <?php $this->need('inc/head.php');
  $this->need('inc/header.php'); ?>
