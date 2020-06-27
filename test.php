d05da7208549b338516ebee054e6c4df52971824
kaygb <kaygbtop@163.com>
2020-06-27 15:23:50 +0800

    测试JSON

--- test.php
+++ test.php
@@ -19,15 +19,17 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 </style>
 <div style="max-width:800px;margin:0 auto;">
 <h1 style="background:#eee;padding:20px;">JSON测试页面</h1>
-
-
+<?php
+//    $json = '{"name":"github","url":"//github.com","icon":"fa fa-test"},{"name":"bilibili","url":"//bilibili.com","icon":"fa fa-test"}';
+   $json = '{"name":"github","url":"//github.com","icon":"fa fa-testt"}';
+?>
 <div class="result">
-<span>JSON数据：{"name":"github","url":"//github.com","icon":"fa fa-test"}</span>
+<span>JSON数据：<?php echo $json ?></span>
 <h2>结果：</h2>
 
 <?php
 //    $json = '{"name":"github","url":"//github.com","icon":"fa fa-test"},{"name":"bilibili","url":"//bilibili.com","icon":"fa fa-test"}';
-   $json = '{"name":"github","url":"//github.com","icon":"fa fa-test"}';
+//    $json = '{"name":"github","url":"//github.com","icon":"fa fa-test"}';
 // winds_procard_item($this->options->windsProCardItem);
 winds_procard_item($json);
 
