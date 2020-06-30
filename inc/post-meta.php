ea6acab2d4b3ee5006982d358b08bd942509f57b
kaygb <kaygbtop@163.com>
2020-06-30 21:08:01 +0800

    Update post-meta.php

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -2,6 +2,7 @@
 // post-meta
 ?>
 
+<?php if($this->is('post')):?>
 <div class="post-meta">
         <span class="">  
             <?php _e('<i class="fas fa-clock"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
@@ -26,3 +27,20 @@
             </span>
             <?php endif;?>
     </div>
+         <?php else: ?>
+        <div class="post-meta">
+        <span class="">  
+            <?php _e('<i class="fas fa-clock"></i> '); ?><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
+        </span>
+        <span class="">
+            <?php _e('<i class="fas fa-comment-alt"></i> '); ?><?php $this->commentsNum(_t('暂无评论'), _t('1 条评论'), _t(' %d 条评论')); ?>
+        </span>
+        <span><?php _e('<i class="fas fa-eye"></i> '); ?><?php get_post_view($this) ?> 次浏览</span>
+
+         <?php if($this->user->hasLogin()):?>
+            <span><?php _e('<i class="fas fa-edit"></i> '); ?>
+                <a href="<?php $this->options->adminUrl(); ?>write-page.php?cid=<?php echo $this->cid;?>" target="_blank">编辑</a>
+            </span>
+            <?php endif;?>
+    </div>
+         <?php endif;?>
